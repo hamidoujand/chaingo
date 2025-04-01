6adcf17065209fd37af76b2f811a0df6a8ef365c
Hamid Oujand <hmdrzadev@gmail.com>
2025-04-01 16:11:48 +0330

    add the TX type

---
+++ database/database.go
@@ -0,0 +1,57 @@
+package database
+
+import "fmt"
+
+// AccountID represents the last 20 bytes of the public key.
+type AccountID string
+
+func NewAccountID(hex string) (AccountID, error) {
+	a := AccountID(hex)
+	if !a.IsValid() {
+		return "", fmt.Errorf("invalid accountID format")
+	}
+	return a, nil
+}
+
+func (id AccountID) IsValid() bool {
+	const addressLength = 20
+	if has0xPrefix(id) {
+		id = id[2:] //remove the prefix then.
+	}
+
+	return len(id) == 2*addressLength && isHex(id)
+}
+
+func has0xPrefix(id AccountID) bool {
+	return len(id) >= 2 && id[0] == '0' && (id[1] == 'x' || id[1] == 'X')
+}
+
+func isHex(id AccountID) bool {
+	if len(id)%2 != 0 {
+		return false
+	}
+
+	for _, c := range []byte(id) {
+		if !isHexCharacter(c) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isHexCharacter(c byte) bool {
+	return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')
+}
+
+//==============================================================================
+
+type TX struct {
+	ChainID uint16    `json:"chain_id"` //Prevents replay attacks across different blockchains.
+	Nonce   uint64    `json:"nonce"`    //Acts as a unique counter for transactions from the same account and ensures transaction ordering.
+	FromID  AccountID `json:"from"`     //Nodes verify that the signature matches this address (to prevent impersonation).
+	ToID    AccountID `json:"to"`       //The recipient’s account address.
+	Value   uint64    `json:"value"`    //The amount of cryptocurrency being sent.
+	Tip     uint64    `json:"tip"`      //A priority fee (tip) to incentivize miners to include this transaction faster.
+	Data    []byte    `json:"data"`     //Stores arbitrary data.
+}
